2f38d35aaee6c174cffeae75a2780a7cfcb10c54
Rishav Kumar <rishavkumar2k07@gmail.com>
2026-09-09 17:16:38 +0530

    Time: 1 ms (8.53%), Space: 9.1 MB (57.33%) - LeetHub

--- 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.cpp
@@ -10,14 +10,37 @@ public:
     //     return multiplier;
     // }
 
-    long long countCommas(long long n) 
-    {
+        // long long l = 0, temp = n;
+        // while(temp)
+        // {
+        //     temp /= 10;
+        //     l++;
+        // }
+        // long long result = 0;
+        // if (l <= 3) 
+        //     result = 0;
+
+        // else if (l <= 6) 
+        //     result = n - 999;
+        // else if (l <= 9)
+        //     result = n - 999 + commas(9) * (n - 999999);
+        // else if (l <= 12)
+        //     result = n - 999 + commas(9) * (n - 999999) + commas(12) * (n - 999999999);
+        // else if (l <= 15)
+        //     result = n - 999 + commas(9) * (n - 999999) + commas(12) * (n - 999999999) + commas(15) * (n - 999999999999);
+        // else 
+        //     result = n - 999 + commas(9) * (n - 999999) + commas(12) * (n - 999999999) + commas(15) * (n - 999999999999) + commas(l);
+
+
+
+    // above idea in terms of loop!!
+    long long countCommas(long long n) {
         long long result = 0;
         for (long long p = 1000; p <= n; p *= 1000)
         {
             result += n - (p - 1);
         }
-
+        
         return result;
         
     }
